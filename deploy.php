664242c3a3a7a590f056f50aaf908f60f3bf3916
dkoehl <gitkraken@pm-newmedia.com>
2020-04-02 17:06:39 +0200

    composer angepasst

--- deploy.php
+++ deploy.php
@@ -32,6 +32,7 @@ host('boardinghouse.westeurope.cloudapp.azure.com')
 
 task('deploy', [
     'setPerms',
+    'setGroup',
     'deploy:info',
     'deploy:prepare',
     'deploy:lock',
@@ -48,6 +49,7 @@ task('deploy', [
     'deploy:unlock',
     'cleanup',
     'setPerms',
+    'setGroup',
     'success'
 ]);
 task('composer install', 'composer update');
@@ -55,5 +57,6 @@ task('yarn install', '/usr/bin/yarn install');
 task('yarn encore', '/usr/bin/yarn encore dev');
 task('clear cache', 'php bin/console cache:clear');
 task('setPerms', 'chmod -R 0777 /var/www/booking/current/');
+task('setGroup', 'chown -R dkoehl:www-data /var/www/booking/');
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
