e4375457827d90013b84a196f751ebebffb6ad1e
Dennis Köhl <dennis.koehl@diva-e.com>
2020-05-05 12:50:31 +0200

    clean up

--- assets/js/components/checkout/checkout_send.js
+++ assets/js/components/checkout/checkout_send.js
@@ -35,38 +35,3 @@ if (generatePDFButton) {
 
     });
 }
-
-// if (generatePDFButton) {
-//     let modalPrintButton = document.getElementById('generatePDFs').addEventListener('click', function () {
-//         // getting print- formula data
-//         let data = document.querySelector('#generatePDFs');
-//         // send formular data to booking- controller for generating pdf files
-//         axios.post('/booking/checkout/sendCheckoutFiles', {
-//             bookingid: data.dataset.bookingid
-//         })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     });
-// }
-// if (modalPrint) {
-//     let modalPrintButton = document.getElementById('modalprilnt_button').addEventListener('click', function () {
-//         // getting print- formula data
-//         let elements = document.getElementById("modalprintformular").elements;
-//         let printFormularData = {};
-//         for (let i = 0; i < elements.length; i++) {
-//             let item = elements.item(i);
-//             if (item.checked) {
-//                 printFormularData[item.name] = true;
-//             }
-//             if (item.name === 'bookingid') {
-//                 printFormularData[item.name] = item.value;
-//             }
-//         }
-//         // send formular data to booking- controller for generating pdf files
-//         axios.post('/booking/checkout/print/', printFormularData)
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     });
-// }
